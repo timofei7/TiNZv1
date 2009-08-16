f558929146016a061d1e793d53ce2ad71a3f598f
Tim Tregubov <tim@zingweb.com>
2009-08-16 16:43:09 +0000

    redid some of leddriver and now its broken but closer to what its supposed to do stupid SCLK

--- cellHelp/LEDDriver.vhd
+++ cellHelp/LEDDriver.vhd
@@ -33,146 +33,133 @@ end LEDDriver;
 
 architecture Behavioral of LEDDriver is
 
-COMPONENT ClockDivider
-GENERIC (DIV : integer := 2); --just send a number like 25 for 1hz etc.. 
-PORT(
-   Clk : IN std_logic;          
-   slowCE : OUT std_logic
-   );
-END COMPONENT;
-
-
 constant teststring: std_logic_vector(511 downto 0) := ("00000000011011110100101011111111100000001110000001100011111000110001000000011100100100001111110000000001000000110000101000011111000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
    );
 signal shiftreg: std_logic_vector(511 downto 0) := ("00000000011011110100101011111111100000001110000001100011111000110001000000011100100100001111110000000001000000110000101000011111000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
    );
-signal slowCE: std_logic :='0';   
-
+   
 -- state machine for SPI bus
---type state_type is (sIdle, sInit, sWaitS, sSend, sDeInit, sWaitD);	-- state machine
-type state_type is (sIdle, sInit, sWaitS, sSend);	-- state machine
+type state_type is (sIdle, sInit, sWaitS, sSendInit, sSendDown, sSendM, sSendUP, sDeInit, sWaitD);	-- state machine
 signal curr_state, next_state: state_type;
 
 --constant waitFinal: unsigned(14 downto 0) := "110000110101000"; -- 25,000 * 20ns = .5ms delay 
 constant waitFinal: unsigned(14 downto 0) := "000000000000100"; -- FOR TESTING
-signal waitCounter: unsigned(14 downto 0) := (others => '0'); --counts for the initial wait
+signal waitCount: unsigned(14 downto 0) := (others => '0'); --counts for the initial wait
 signal waitTC : std_logic:='0'; --terminal count
-signal waitTCreg : std_logic:='0'; --register it cause it only lasts one clock cycle not long enough
 signal waitReset : std_logic:='0'; --reset the wait counter
 
-constant shiftFinal: unsigned(8 downto 0):= "111111111"; --final count 511
-signal shiftCounter: unsigned(8 downto 0):= (others => '0'); --count shifts
-signal shiftTC: std_logic:='0'; --terminal count for shifting all 512 bits
+constant slowFinal: unsigned(7 downto 0) := "11111010"; --count to 250 for a rate of 100khz in for down/up of SCLK
+signal slowCount: unsigned(7 downto 0) := (others => '0'); --counts for SCLK
+signal slowTC : std_logic:='0'; --terminal count
+signal slowReset : std_logic:='0'; --reset the wait counters
 
-signal shiftBit: std_logic:='0'; --shift our bits out onto MOSI
-signal godisp: std_logic:='0'; --we register GoDisplay since it is only on for a short clock cycle
+constant shiftFinal: unsigned(8 downto 0):= "111111111"; --final count 511
+signal shiftCount: unsigned(8 downto 0):= (others => '0'); --count shifts
+signal shiftReset: std_logic:='0'; --reset shift counter
+signal shiftBit: std_logic:='0'; --shift our bits out onto MOSIs
+
+signal slowSCLK : std_logic:='0'; --internal slow SCLK signal
    
 begin
 
-
-spclkdiv: ClockDivider PORT MAP(
-		Clk => Clk,
-		slowCE => slowCE
-	);
-   
-SCLK <= slowCE; --send the clock out on the SCLK pin
-
+SCLK <= slowSCLK;
 MOSI <= shiftreg(0); --we shift out datas from this shift register
 
-waitCount: --15bit up counter to enable .5ms pause
+waitCounter: --15bit up counter to enable .5ms pause
 process(Clk)
    begin
       if rising_edge(Clk) then
          if waitReset = '1' then
-            waitCounter <= (others => '0');
+            waitCount <= (others => '0');
          else
-            waitCounter <= waitCounter + 1;
+            waitCount <= waitCount + 1;
          end if;
       end if;
-end process waitCount;
-waitTC <= '1' when waitCounter = waitFinal else '0';
+end process waitCounter;
+waitTC <= '1' when waitCount = waitFinal else '0';
 
-waitreg: --register the terminal counter of wait count cause otherwise it goes by too fast
+
+slowCounter: --15bit up counter to enable .5ms pause
 process(Clk)
    begin
       if rising_edge(Clk) then
-         if waitTC = '1' then
-            waittcreg <= '1';
-         elsif waitReset = '1' then
-            waittcreg <= '0';
+         if slowReset = '1' then
+            slowCount <= (others => '0');
+         else
+            slowCount <= slowCount + 1;
          end if;
       end if;
-end process waitreg;
+end process slowCounter;
+slowTC <= '1' when slowCount = slowFinal else '0';
 
-godispreg: --register godisplay input otherwise the 1 clock pulse goes by too fast
+
+shiftCounter: --output shift register! --perhaps combine with DISPLAY output shift register
 process(Clk)
    begin
       if rising_edge(Clk) then
-         if GoDisplay = '1' then
-            godisp <= '1';
-         elsif shiftTC = '1' then
-            godisp <= '0';
-         end if;
-      end if;
-end process godispreg;
-
-
-
-shiftbits: --output shift register! --perhaps combine with DISPLAY output shift register
-process(Clk)
-   begin
-      if rising_edge(Clk) and slowCE = '1' then --do i need slowCE here
-         if shiftBit = '1' then
-            shiftreg <=  DATA & shiftreg(511 downto 1); 
-            shiftCounter <= shiftCounter + 1;
-         elsif shiftTC = '1' then
+         if shiftReset = '1' then
             shiftreg <= teststring;
-            shiftCounter <= (others => '0');
+            shiftCount <= (others => '0');
+         elsif shiftBit = '1' then
+            shiftreg <=  DATA & shiftreg(511 downto 1); --what DATA?!? huh
+            shiftCount <= shiftCount + 1;
          end if;
       end if;
-end process shiftbits;
-shiftTC <= '1' when shiftCounter = shiftFinal else '0';
-
+end process shiftCounter;
 
 statemachine: --state machine to do all the work
-process(curr_state, godisp, waitTCreg, shiftTC)
+process(curr_state, GoDisplay, waitTC, shiftCount, slowTC, slowCount)
    begin
    next_state <= curr_state;
-   CS <= '1';
+   CS <= '0'; --opposite default - this is active low but we only set it high false during sIdle
    shiftBit <= '0';
    waitReset <= '0';
+   slowReset <= '0';
+   slowSCLK <= '0';
+   shiftReset <= '0';
    
    case curr_state is
-      when sIdle =>
-         if godisp = '1' then
+      when sIdle => --base state - CS is high false
+         CS <= '1';
+         if GoDisplay = '1' then
             next_state <= sInit;
          end if;
-      when sInit =>
-         CS <= '0';
+      when sInit => --resest counters to deassert cs and be ready for shifting
+         CS <= '1';
          waitReset <= '1';
+         shiftReset <= '1';
          next_state <= sWaitS;
-      when sWaitS =>
-         CS <= '0'; --active low about to send data
-         if waitTCreg = '1' then
-            next_state <= sSend;
+      when sWaitS => --wait for .5ms after deasserting cs
+         if waitTC = '1' then
+            next_state <= sSendInit;
          end if;
-      when sSend =>
+      when sSendInit => --reset counts to initialize sending cycle
+         slowReset <= '1';
+         shiftbit <= '1';
+         next_state <= sSendDown;
+      when sSendDown => --keep SCLK low for half an SCLK cycle
+         if slowTC = '1' then
+            next_state <= sSendM;
+         end if;
+      when sSendM => --reset slow counter
+         slowReset <= '1';
+         next_state <= sSendUP;
+      when sSendUP =>
+         slowSCLK <= '1'; --default is 0 so here's the rising edge for the slave!
+         if slowTC = '1' then
+            if slowCount = slowFinal then --are we done shifting?
+               next_state <= sDeInit;
+            else
+               next_state <= sSendInit;
+            end if;
+         end if;
+      when sDeInit => --NOT SURE WE NEED DELAY AT END --this is for CS to stay low for a little extra
          waitReset <= '1';
-         CS <= '0';
-         if shiftTC = '1' then
+         next_state <= sWaitD;
+      when sWaitD => --wait for .5ms to denable cs
+         if waitTC = '1' then
             next_state <= sIdle;
-         else
-            shiftBit <= '1';
          end if;
---      when sDeInit => --NOT SURE WE NEED DELAY AT END TRY WITHOUT IT
---         CS <= '0';
---         waitReset <= '1';
---         next_state <= sWaitD;
---      when sWaitD =>
---         CS <= '0';
---         if waitTCreg = '1' then
---            next_state <= sIdle;
---         end if;
       when others => next_state <= sIdle;
    end case;
 end process statemachine;
@@ -181,7 +168,7 @@ end process statemachine;
 statechanger:
 process (Clk)
 begin
-	if rising_edge(Clk) and slowCE = '1' then --slow us down to at most 125khz
+	if rising_edge(Clk) then --slow us down to at most 125khz
 		curr_state <= next_state;
 	end if;
 end process statechanger;
