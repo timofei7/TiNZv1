92fac335a00712d88f8a4f8ff48c43ba9655bf41
Tim Tregubov <tim@zingweb.com>
2009-08-22 02:03:49 +0000

    testcoe

--- coeGenerator/genCOE.py
+++ coeGenerator/genCOE.py
@@ -7,14 +7,14 @@ DEBUG=False
 PREFIX=";; this is memory inits for %s\nmemory_initialization_radix=2;\nmemory_initialization_vector="
 
 # from http://en.wikipedia.org/wiki/Web_colors#Web-safe_colors bottom of page
-COLORS = {"black":"00000000","gray":"01101111","silver":"01001010","white":"11111111","maroon":"10000000","red":"11100000",
-	"purple":"01100011","fuchsia":"11100011","green":"00010000","lime":"00011100","olive":"10010000",
+COLORS = {"black":"00000000","gray":"01101110","silver":"01001001","white":"11111111","maroon":"10000000","red":"11100000",
+	"purple":"01100011","fuchsia":"11100011","green":"00010100","lime":"00011100","olive":"10010000",
 	"yellow":"11111100","navy":"00000001","blue":"00000011","teal":"00001010","aqua":"00011111"}
 # this is what we use in the definition files
 COLORMAP = {"X":"black","E":"grey","S":"silver","W":"white","M":"maroon","R":"red","P":"purple","F":"fuchsia","G":"green","L":"lime","O":"olive","Y":"yellow","N":"navy","B":"blue","T":"teal","A":"aqua"}
 
 # [1 bit occupied][1 bit enemy or powerup][6 bit identifier]
-idents ={"enemy": "000001", "powerup": "000010", "blank": "000000"}
+idents ={"enemy1":"000001", "enemy2":"000010", "enemy3":"000011", "powerup1":"000100", "powerup2":"000101", "powerup3":"000110", "finish":"000111","blank": "000000"}
 
 if len(sys.argv) > 1:
 	file = sys.argv[1]
@@ -27,7 +27,7 @@ lines = open(file).readlines()
 
 output = []
 
-row = 0
+row = 7
 frame = 0
 col = 0
 frametotal=0
@@ -52,7 +52,7 @@ def countframes():
 def comma():
 	global row, col
 	cm = ""
-	if row == 7 and col == 7 and frame == frametotal:
+	if row == 0 and col == 7 and frame == frametotal:
 		cm = ";"
 	else:
 		cm = ","
@@ -66,9 +66,19 @@ def comma():
 def encodePositional(char):
 	global output, col 
 	if char=="X":
-		output.append("11" + idents["enemy"] + comma())	
+		output.append("11" + idents["enemy1"] + comma())	
+	elif char=="Y":
+		output.append("11" + idents["enemy2"] + comma())	
+	elif char=="Z":
+		output.append("11" + idents["enemy3"] + comma())	
 	elif char=="P":
-		output.append("10" + idents["powerup"] + comma())	
+		output.append("10" + idents["powerup1"] + comma())	
+	elif char=="Q":
+		output.append("10" + idents["powerup2"] + comma())	
+	elif char=="R":
+		output.append("10" + idents["powerup3"] + comma())	
+	elif char=="F":
+		output.append("00" + idents["finish"] + comma())
 	elif char=="O":
 		output.append("00" + idents["blank"] + comma())
 	#if space do nothing
@@ -90,7 +100,7 @@ for line in lines:
 			if frame != 0:
 				output.append(",\n")
 		frame = frame+1	
-		row = 0 #reset row count 
+		row = 7 #reset row count 
 	elif line != "\n": #skip blank lines
 		if DEBUG: output.append(" #row: %s " % row)
 		for char in list(line):
@@ -98,7 +108,7 @@ for line in lines:
 				encodePositional(char)		
 			elif mode=="coloral":
 				encodeColoral(char)
-		row = row+1
+		row = row-1
 
 #print it all to a .coe file
 newfile = open(file+".coe", "w")
